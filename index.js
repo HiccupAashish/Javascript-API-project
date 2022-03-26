d4298c2be46665f4ae3b1d8d27b94d623911d9db
HiccupAashih <shahaashish22@gmail.com>
2022-03-26 14:11:17 +1100

    Like button not working

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const politics = document.querySelector("li#politics");
 const modalp = document.querySelector("#modalp");
 const modal = document.querySelector("#modal");
 const p = document.querySelector("#like");
+const like = document.querySelector("#liker")
 const wholebody = document.querySelector("#whole-body");
 const btnclose = document.querySelector("#close");
 const modalheading = document.querySelector("#headingofmodal");
@@ -21,8 +22,9 @@ const nextBtn = document.querySelector(".nextBtn");
 const heartbar = document.querySelector("#heart");
 const heart = document.querySelector(".fa-solid");
 const thankyou = document.querySelector("#thankyou");
+// let cat = "india_english_sports";
 const API =
-    "https://newsapi.in/newsapi/news.php?key=5PqRdw8t9TiQBKfPMpXnFrDAScMF8c&category=india_english";
+    `https://newsapi.in/newsapi/news.php?key=5PqRdw8t9TiQBKfPMpXnFrDAScMF8c&category=`;
 let page = 1;
 
 navdisplay.addEventListener("click", (event) => {
@@ -41,9 +43,12 @@ navbar.addEventListener("click", (event) => {
     const tg = event.target;
     if (tg.id === "science") {
         float.innerHTML = `<marquee scrollamount="12">Science</marquee> `;
-        fetchNewsDetails(
-            "https://newsapi.in/newsapi/news.php?key=5PqRdw8t9TiQBKfPMpXnFrDAScMF8c&category=india_english_science"
-        );
+
+        fetchNewsDetails(API + "india_english_science")
+            // fetchNewsDetails(
+            //     // "https://newsapi.in/newsapi/news.php?key=5PqRdw8t9TiQBKfPMpXnFrDAScMF8c&category=india_english_science"
+            //     cat="india_english_science";
+            // );
     } else if (tg.id === "sports") {
         float.innerHTML = `<marquee scrollamount="12">Sports </marquee> `;
         fetchNewsDetails(
@@ -65,7 +70,9 @@ navbar.addEventListener("click", (event) => {
 let news;
 async function fetchNewsDetails(url) {
     try {
-        const response = await fetch(`${url}`);
+        console.log(url)
+        const response = await fetch(url);
+        // console.log(response)
         news = (await response.json()).News;
         const first = news.slice(0, 10);
         renderdata(first);
@@ -75,31 +82,39 @@ async function fetchNewsDetails(url) {
 }
 
 function renderdata(data) {
-    const d = data.map((char) => {
-        return `<div  data-character='${JSON.stringify(char)}' class="card"> 
-    <h2 data-character='${JSON.stringify(char)}'>${char.title}</h2>
-    <img data-character='${JSON.stringify(char)}' src="${
-      char.image
+
+    const d = data.map((article) => {
+        return `<div  data-article='${JSON.stringify(article)}' class="card"> 
+    <h2 data-article='${JSON.stringify(article)}'>${article.title}</h2>
+    <img data-article='${JSON.stringify(article)}' src="${
+      article.image
     }" alt="No picture available"style="float:left; width:120px;height:100px;">
-    <p data-character='${JSON.stringify(
-      char
-    )}' id="newsdescription" style="float:right;">${char.description}</p>
-    <p data-character='${JSON.stringify(
-      char
-    )}' id="newsurl"class="hide" class>${char.url}</p>
-    <p data-character='${JSON.stringify(
-      char
-    )}' id="newspublished">Date published: ${char.published_date}</p>
-    </div>`;
+    <p data-article='${JSON.stringify(
+      article
+    )}' id="newsdescription" style="float:right;">${article.description}</p>
+    <p data-article='${JSON.stringify(
+      article
+    )}' id="newsurl"class="hide" class>${article.url}</p>
+    <p data-article='${JSON.stringify(
+ article
+    )}' id="newspublished">Date published: ${article.published_date}</p>
+    <div id="liker" >
+   <i class="fa-solid fa-heart"></i>
+   </div>
+   
+    </div>
+    >`;
     });
     content.innerHTML = d.join("");
+    console.log(like)
+
 }
 
 content.addEventListener("click", (event) => {
     const trigger = event.target;
 
     try {
-        const char = JSON.parse(trigger.dataset.character);
+        const char = JSON.parse(trigger.dataset.article);
         modalheading.innerHTML = `${char.title}`;
 
         modalp.innerHTML = ` ${char.description}`;
@@ -143,15 +158,20 @@ footer.addEventListener("click", (event) => {
     }
 });
 
-heartbar.addEventListener("click", (event) => {
-    if (event.target === heart) {
-        heart.classList.toggle("color");
-        if (p.innerHTML === "Give us a like !") {
-            p.innerHTML = "Thankyou for your love and support";
-        } else {
-            p.innerHTML = "Give us a like !";
-        }
-    }
-});
+// heartbar.addEventListener("click", (event) => {
+//     if (event.target === heart) {
+//         heart.classList.toggle("color");
+//         if (p.innerHTML === "Give us a like !") {
+//             p.innerHTML = "Thankyou for your love and support";
+//         } else {
+//             p.innerHTML = "Give us a like !";
+//         }
+//     }
+// // });
+// like.addEventListener('click', () => {
+//             // heart.classList.toggle('color');
+//             console.log("Aashish")
+// })
 
-fetchNewsDetails(API);
+// console.log(like)
+fetchNewsDetails(API + "india_english");
